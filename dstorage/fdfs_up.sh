ac4847dc96c360b207aa67c6c863ccbb52e0a9b6
liunianshiwei <chizhou_wangjun@163.com>
2016-12-20 19:17:07 +0800

    fdfs启动脚本

--- dstorage/fdfs_up.sh
+++ dstorage/fdfs_up.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+passwd="axposf1.1"
 trackerd_PID=`ps -u  root| grep fdfs_trackerd | awk {'print $1'}`
 echo $trackerd_PID
 if	[ -z $trackerd_PID ]
-	sudo kill -9 $trackerd_PID
-	sudo fdfs_trackerd /etc/fdfs/tracker.conf
+	echo $passwd | sudo -S kill -9 $trackerd_PID
+	echo $passwd | sudo -S fdfs_trackerd /etc/fdfs/tracker.conf
 then
-	sudo fdfs_trackerd /etc/fdfs/tracker.conf
+	echo $passwd | sudo -S fdfs_trackerd /etc/fdfs/tracker.conf
 fi
 echo "start fdfs_trackerd sucess"
 
@@ -14,9 +14,9 @@ echo "start fdfs_trackerd sucess"
 storaged_PID=`ps -u  root| grep fdfs_storaged | awk {'print $1'}`
 echo $storaged_PID
 if  [ -z $storaged_PID ]
-	sudo kill -9 $storaged_PID
-	sudo fdfs_storaged /etc/fdfs/storage.conf
+	echo $passwd | sudo -S kill -9 $storaged_PID
+	echo $passwd | sudo -S fdfs_storaged /etc/fdfs/storage.conf
 then
-	sudo fdfs_storaged /etc/fdfs/storage.conf
+	echo $passwd | sudo -S fdfs_storaged /etc/fdfs/storage.conf
 fi
 echo "start fdfs_storaged sucess"
